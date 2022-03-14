7c7ffcd32fe7435fc680f43307e50f5ac4acd1f8
Tyler <45154554+santost12@users.noreply.github.com>
2022-03-14 16:32:56 -0400

    Update encode-av1.sh

--- encode-av1.sh
+++ encode-av1.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# See: https://ffmpeg.org/trac/ffmpeg/wiki/CompilationGuide
 ffmpegBinary="/home/ffmpeg/bin/ffmpeg"
 
+# Change these for your setup
 inputDir="/home/tyler/handbrake-input/"
 outputDir="/home/tyler/hrandbrake-output/"
 
 fileExt="*.mkv"
-
 counter=1
+
+# Change the ffmpeg arguments if you want
+# See: https://trac.ffmpeg.org/wiki/Encode/AV1
 
 for file in $inputDir$fileExt; do
 	"$ffmpegBinary" -i "$file" -c:v libaom-av1 -crf 30 -pix_fmt yuv420p10le -b:v 0 -cpu-used 4 -c:a libopus -b:a 128k "$outputDir$counter.mkv"
