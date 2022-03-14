68f9aba132f60ace4ff21c70857cc7e6a4c44b4b
Tyler <45154554+santost12@users.noreply.github.com>
2022-03-14 16:42:04 -0400

    Update encode-av1.sh

--- encode-av1.sh
+++ encode-av1.sh
@@ -12,8 +12,9 @@ counter=1
 
 # Change the ffmpeg arguments if you want
 # See: https://trac.ffmpeg.org/wiki/Encode/AV1
+ffmpegArguments="-c:v libaom-av1 -crf 30 -pix_fmt yuv420p10le -b:v 0 -cpu-used 4 -c:a libopus -b:a 128k"
 
 for file in $inputDir$fileExt; do
-	"$ffmpegBinary" -i "$file" -c:v libaom-av1 -crf 30 -pix_fmt yuv420p10le -b:v 0 -cpu-used 4 -c:a libopus -b:a 128k "$outputDir$counter.mkv"
+	"$ffmpegBinary" -i "$file" $ffmpegArguments "$outputDir$counter.mkv"
 	counter=$((counter+1))
 done
