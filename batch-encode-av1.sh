250fea1ab055832c52dc55a5824bd14245f5bd7b
santost12 <45154554+santost12@users.noreply.github.com>
2022-06-08 14:46:02 -0400

    Update batch-encode-av1.sh

--- batch-encode-av1.sh
+++ batch-encode-av1.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# See: https://ffmpeg.org/trac/ffmpeg/wiki/CompilationGuide
-ffmpegBinary="/home/ffmpeg/bin/ffmpeg"
+## Note: This script assumes your videos are:
+##  1) in order
+##  2) using MKV container
 
-# Change these for your setup
-inputDir="/home/tyler/handbrake-input/"
-outputDir="/home/tyler/hrandbrake-output/"
+## Download ffmpeg or see: https://ffmpeg.org/trac/ffmpeg/wiki/CompilationGuide
+ffmpegBinary="/c/Users/tyler/bin/ffmpeg-2022-06-06-git-73302aa193-full_build/bin/ffmpeg.exe"
 
-fileExt="*.mkv"
+## Change these to match your source and output folders
+inputDir="/c/Users/tyler/handbrake-input/*.mkv"
+outputDir="/c/Users/tyler/handbrake-output/"
 counter=1
 
-# Change the ffmpeg arguments if you want
-# See: https://trac.ffmpeg.org/wiki/Encode/AV1
+## See: https://trac.ffmpeg.org/wiki/Encode/AV1
 ffmpegArguments="-c:v libaom-av1 -crf 30 -pix_fmt yuv420p10le -b:v 0 -cpu-used 4 -c:a libopus -b:a 128k"
 
-for file in $inputDir$fileExt; do
-	"$ffmpegBinary" -i "$file" $ffmpegArguments "$outputDir$counter.mkv"
-	counter=$((counter+1))
+for file in $inputDir; do
+
+    if [ $counter -lt 10 ]; then
+        "$ffmpegBinary" -i "$file" $ffmpegArguments "$outputDir"0"$counter.mkv"
+    else
+        "$ffmpegBinary" -i "$file" $ffmpegArguments "$outputDir$counter.mkv"
+    fi
+
+    counter=$((counter+1))
 done
